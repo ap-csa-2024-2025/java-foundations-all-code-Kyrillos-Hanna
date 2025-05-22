9d6db8d7e4c6b07f2f52e2cc3d7ef0c720840690
Kyrillos-Hanna <kyrohanna90@gmail.com>
2025-05-22 11:21:05 -0700

    did work

--- src/jfo-section-1/HelloWorld.java
+++ src/jfo-section-1/HelloWorld.java
@@ -1,6 +1,12 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your grade");
+        int huzz = sc.nextInt();
+        System.out.println("your grade next year will be " + (++huzz));
     }
 }
